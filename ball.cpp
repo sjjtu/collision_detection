27b50b8668c6686ade9282c8653d7c98d784ffab
Wille Wiman <willewiman@eduroam-10-200-45-135.local.su.se>
2023-04-15 17:55:58 +0200

    johhny boy

---
+++ ball.cpp
@@ -0,0 +1,19 @@
+ #include "ball.h"
+ 
+ Ball::Ball(float r , float m, float pos_x, float pos_y, float vel_x, float vel_y){
+            radius = r;
+            mass = m;
+            position_x = pos_x;
+            position_y = pos_y;
+            velocity_y = vel_y;
+            velocity_x = vel_x;
+        }
+void Ball::update(float dt){
+    this->position_x = position_x + dt*velocity_x;
+    this->position_y = position_y + dt*velocity_y;
+};
+
+
+
+
+
---
+++ ball.h
@@ -0,0 +1,16 @@
+class Ball {
+    public:
+        float radius;
+        float mass;
+        float position_x;
+        float position_y;
+        float velocity_x;
+        float velocity_y;
+        Ball(float r , float m, float pos_x, float pos_y, float vel_x, float vel_y);
+
+        void update(float dt);  
+            
+
+        bool collision_detected();
+
+};
